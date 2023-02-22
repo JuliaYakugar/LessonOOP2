03c046753020c0b5cf11ceb1d037235e202c52e3
JuliaYakugar <88778090+JuliaYakugar@users.noreply.github.com>
2023-02-22 04:37:09 +0400

    Less 02

---
+++ Less_02/Main.java
@@ -0,0 +1,42 @@
+package Less_02;
+
+import java.util.ArrayList;
+
+import Less_02.animals.Lion;
+import Less_02.animals.Wolf;
+import Less_02.cage.LionCage;
+import Less_02.cage.WolfCage;
+import Less_02.factory.LionsFactory;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        Lion lionAva = new Lion(2010, 14, 4, 500);
+        System.out.println("Лев Ава до кормления: " + lionAva);
+        lionAva.feed(2);
+        System.out.println("Лев Ава после кормления (+ 2кг): " + lionAva);
+
+        lionAva.feed(2); 
+
+        ArrayList<Lion> lions = LionsFactory.createLions(5);
+
+        LionCage lc = new LionCage(lions);
+        System.out.println(lc);
+        System.out.println("===========");
+
+        System.out.println(lc.deliverFood(100));
+        System.out.println("===========");
+
+        System.out.println(lc);
+        System.out.println("===========");
+
+        WolfCage goodWolfs = new WolfCage();
+        goodWolfs.addAnimal(new Wolf(3, 9, 4, "Белый"));
+        goodWolfs.addAnimal(new Wolf(3, 9, 4, "Черный"));
+        goodWolfs.addAnimal(new Wolf(3, 9, 4, "Серый"));
+
+        System.out.println(goodWolfs.randomAnimal());
+
+    }
+}
---
+++ Less_02/factory/LionsFactory.java
@@ -0,0 +1,16 @@
+package Less_02.factory;
+
+import java.util.ArrayList;
+
+import Less_02.animals.Lion;
+
+public class LionsFactory {
+    
+    public static ArrayList<Lion> createLions(int countLions) {
+        ArrayList<Lion> lionsFactory = new ArrayList<>(countLions);
+        for (int i = 0; i < countLions; i++) {
+            lionsFactory.add(new Lion(10, 10, 10, 10));
+        }
+        return lionsFactory;
+    }
+}
